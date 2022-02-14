dfa1f6de2128eeed8a2c56b11349cf33ce11b152
TanveerS1ngh <95960398+TanveerS1ngh@users.noreply.github.com>
2022-02-14 17:57:30 +0530

    last question of chapter one

--- Nichola-Lacey-Python-Questions/Challenges_1-11:The_Basics/Question_011.py
+++ Nichola-Lacey-Python-Questions/Challenges_1-11:The_Basics/Question_011.py
@@ -1,2 +1,6 @@
 # Task the user to enter a number over 100 and then enter a number under 10 and tell them how many times the smaller number goes into the larger number in a user-friendly format.
 
+user_no1 = int(input("Enter a number over 100 > "))
+user_no2 = int(input("Enter a number under 10 > "))
+
+print(f"You have to add {user_no2} {user_no1//user_no2} times to make {user_no1}")
