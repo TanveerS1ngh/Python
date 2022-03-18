31c100978493b1d3b313c5c43137884385d5e73a
TanveerS1ngh <95960398+TanveerS1ngh@users.noreply.github.com>
2022-03-18 15:08:29 +0530

    .

--- Pythonwithmosh/4.Data_structure.py
+++ Pythonwithmosh/4.Data_structure.py
@@ -3,10 +3,11 @@
 word = ["a", "b", "c", "d", "e", "f", "g", "h"]
 matrix = [[2, 8], [6, 9], [5, 7]]
 lst = [1] * 100  # dup the list values using *
-char = list["hello world"]
+char = list["helloworld"]
 print(word + lst)
 
 # accessing items in list
 
 latr = ["a", "b", "c", "d", "e", "f"]
 print(latr[1:5])
+print(latr[:5])
