77a6ccf9b6759d2754fc82bff84655d5c216e904
TanveerS1ngh <95960398+TanveerS1ngh@users.noreply.github.com>
2022-03-19 13:29:50 +0530

    .

--- Pythonwithmosh/4.Data_structure.py
+++ Pythonwithmosh/4.Data_structure.py
@@ -3,11 +3,14 @@
 word = ["a", "b", "c", "d", "e", "f", "g", "h"]
 matrix = [[2, 8], [6, 9], [5, 7]]
 lst = [1] * 100  # dup the list values using *
-char = list["helloworld"]
-print(word + lst)
+char = list("hello world")
+print(word + lst + char)
 
 # accessing items in list
 
 latr = ["a", "b", "c", "d", "e", "f"]
+latr[2] = "C"  # change or updating items in list while previous one will remain there
 print(latr[1:5])
 print(latr[:5])
+print(latr[0:])
+pint
